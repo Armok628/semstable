be56591918b7b6e397d625fa4ebfd3a815058b04
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-11-29 00:38:05 -0600

    Made a couple of adjustments

--- hash.c
+++ hash.c
@@ -6,12 +6,12 @@ typedef struct {
 	int initsize;
 	int **mem; // int could be be changed to anything (even void)
 } hash_t;
-unsigned long hash_key(char *str) // long long could be appropriate
+unsigned long hash_key(char *str) // lazy
 {
 	unsigned long l;
 	for (char *c=str;*c;c++) {
-		l<<=4;
-		l+=*c&15; // four lowest bits -> case insensitive
+		l<<=1;
+		l+=(*c)&31; // case insensitive; suits my needs
 	}
 	return l;
 }
@@ -32,7 +32,7 @@ void set_entry(hash_t *table,char *str,int *entry)
 void add_entry(hash_t *table,char *str,int *entry) // set_entry but handles collisions
 {
 	int *def=table->mem[hash_key(str)%table->size];
-	if (def) { // Hash collision. Double the memory bank to avoid
+	if (def) { // Hash collision. Double the memory bank
 		table->size*=2;
 		table->mem=realloc(table->mem,table->size*sizeof(int *));
 	}
@@ -62,63 +62,26 @@ void free_hash(hash_t *table)
 int main(int argc,char **argv)
 {
 	hash_t *table=new_hash(128);
-	char *input=malloc(100),*number=malloc(100);
+	char *input=malloc(100),*sym=malloc(100);
+	int *num=malloc(sizeof(int));
 	while (strcmp(input,"quit\n")) {
 		printf("Command: ");
 		fgets(input,99,stdin);
-		if (!strcmp(input,"set\n")) {
-			printf("Set what? ");
-			fgets(input,99,stdin);
-			input[strlen(input)-1]='\0';
-			printf("...to what? ");
-			fgets(number,99,stdin);
-			number[strlen(number)-1]='\0';
-			int *num=malloc(sizeof(int));
-			if (sscanf(number,"%i",num)) {
-				printf("Setting %s to %i\n",input,*num);
-				set_entry(table,input,num);
-			}
-		} else if (!strcmp(input,"add\n")) {
-			printf("Add what? ");
-			fgets(input,99,stdin);
-			input[strlen(input)-1]='\0';
-			printf("...as what? ");
-			fgets(number,99,stdin);
-			number[strlen(number)-1]='\0';
-			int *num=malloc(sizeof(int));
-			if (sscanf(number,"%i",num)) {
-				printf("Adding %s as %i\n",input,*num);
-				add_entry(table,input,num);
-			}
-		} else if (!strcmp(input,"get\n")) {
-			printf("Get what? ");
-			input=fgets(input,99,stdin);
-			input[strlen(input)-1]='\0';
-			int *p=get_entry(table,input);
-			if (!p)
-				printf("%s is not defined\n",input);
-			else
-				printf("%i\n",*p);
-		} else if (!strcmp(input,"size\n")) {
-			printf("Current size: %i possible entries\n",table->size);
-		} else {
-			char *sym=malloc(50);
-			int *num=malloc(sizeof(int));
-			if (sscanf(input,"add %s %i",sym,num)==2) {
-				printf("Adding %s as %i\n",sym,*num);
-				add_entry(table,sym,num);
-			} else if (sscanf(input,"set %s %i",sym,num)==2) {
-				printf("Setting %s to %i\n",sym,*num);
-				set_entry(table,sym,num);
-			} else if (sscanf(input,"get %s",sym)==1) {
-				printf("%i\n",*get_entry(table,sym));
-			} else {
-				free(sym);
-				free(num);
-			}
-		}
-
+		if (sscanf(input,"add %s %i",sym,num)==2) {
+			printf("Adding %s as %i\n",sym,*num);
+			add_entry(table,sym,num);
+			num=malloc(sizeof(int));
+		} else if (sscanf(input,"set %s %i",sym,num)==2) {
+			printf("Setting %s to %i\n",sym,*num);
+			set_entry(table,sym,num);
+			num=malloc(sizeof(int));
+		} else if (sscanf(input,"get %s",sym)==1) {
+			printf("%i\n",*get_entry(table,sym));
+			num=malloc(sizeof(int));
+		} else if (!strcmp(input,"size\n"))
+			printf("%i possible entries\n",table->size);
 	}
+	free(num);
 	free_hash(table);
 	return 0;
 }
