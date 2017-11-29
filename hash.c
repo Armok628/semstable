a8c512f22b237e2983cdf838041cc41b2b08a8ba
Mark Johnson <Mark.Johnson.62899@gmail.com>
2017-11-29 13:55:39 -0600

    Improved some things

--- hash.c
+++ hash.c
@@ -4,16 +4,16 @@
 typedef struct {
 	int size; // # of possible entries
 	int initsize;
-	int **mem; // int could be be changed to anything (even void)
+	int **mem; // int could be changed to anything (even void)
 } hash_t;
 unsigned long hash_key(char *str) // lazy
 {
-	unsigned long l=0;
+	unsigned long key=0;
 	for (char *c=str;*c;c++) {
-		l+=(*c)&63;
-		l<<=1;
+		key<<=2;
+		key+=*c;
 	}
-	return l;
+	return key;
 }
 hash_t *new_hash(int size) {
 	hash_t *table=malloc(sizeof(hash_t));
@@ -25,31 +25,35 @@ hash_t *new_hash(int size) {
 void set_entry(hash_t *table,char *str,int *entry)
 {
 	int **def=&table->mem[hash_key(str)%table->size];
-	if (def)
+	if (*def)
 		free(*def);
 	*def=entry;
 }
 void add_entry(hash_t *table,char *str,int *entry) // set_entry but handles collisions
 {
 	int *def=table->mem[hash_key(str)%table->size];
-	if (def) { // Hash collision. Double the memory bank
-		table->size*=2;
+	if (def) { // Hash collision. Double the memory bank and add one
+		table->size+=table->size+1;
 		table->mem=realloc(table->mem,table->size*sizeof(int *));
 	}
 	set_entry(table,str,entry);
 }
 int *get_entry(hash_t *table,char *str)
 {
+	unsigned long key=hash_key(str);
 	int size=table->size;
-	int *def=table->mem[hash_key(str)%size];
+	int *def=table->mem[key%size];
 	if (def)
 		return def;
-	while (!def&&size>table->initsize) // Check possible locations
-		def=table->mem[hash_key(str)%(size/=2)];
+	while (!def&&size>table->initsize) {
+		size--;
+		size/=2;
+		def=table->mem[key%size];
+	}
 	if (!def)
 		return NULL; // Not defined
 	add_entry(table,str,def); // Give it a newer entry
-	table->mem[hash_key(str)%table->size]=NULL; // Remove the old one
+	table->mem[key%table->size]=NULL; // Remove the old one
 	return def;
 }
 void free_hash(hash_t *table)
@@ -57,13 +61,21 @@ void free_hash(hash_t *table)
 	for (int i=0;i<table->size;i++)
 		if (table->mem[i])
 			free(table->mem[i]);
+	free(table->mem);
 	free(table);
 }
 int main(int argc,char **argv)
 {
-	hash_t *table=new_hash(128);
-	char *input=malloc(100),*sym=malloc(100);
+	hash_t *table=new_hash(127);
+	char *input=calloc(100,1),*sym=malloc(100);
 	int *num=malloc(sizeof(int));
+	//////////////////////////////////////////////////////
+	while (strcmp(input,"quit")) {
+		fgets(input,99,stdin);
+		input[strlen(input)-1]='\0';
+		printf("%s -> %lu\n",input,hash_key(input));
+	}
+	//////////////////////////////////////////////////////
 	while (strcmp(input,"quit\n")) {
 		printf("Command: ");
 		fgets(input,99,stdin);
@@ -76,11 +88,16 @@ int main(int argc,char **argv)
 			set_entry(table,sym,num);
 			num=malloc(sizeof(int));
 		} else if (sscanf(input,"get %s",sym)==1) {
-			printf("%i\n",*get_entry(table,sym));
-			num=malloc(sizeof(int));
+			int *ptr=get_entry(table,sym);
+			if (ptr)
+				printf("%i\n",*ptr);
+			else
+				printf("%s is not defined\n",sym);
 		} else if (!strcmp(input,"size\n"))
 			printf("%i possible entries\n",table->size);
 	}
+	free(input);
+	free(sym);
 	free(num);
 	free_hash(table);
 	return 0;
