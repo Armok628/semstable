f80ba34dc7fce7fd137cc5e8b40738777c315ed5
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-08-31 23:38:52 -0500

    Fix invalid writes

--- src/hash.c
+++ src/hash.c
@@ -77,6 +77,7 @@ void insert_bucket(table_t *table,bucket_t *entry)
 				table->destructor(b->val);
 			b->val=entry->val;
 			free(entry);
+			return;
 		} else // Found end
 			b->cdr=entry;
 	} else // No buckets at location
